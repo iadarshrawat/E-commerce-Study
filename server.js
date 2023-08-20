19ac4f398a980008cb02c8607699ad4130e7fd8e
Adarsh rawat <00adarshrawat00@gmail.com>
2023-08-20 16:56:26 +0530

    try another way

--- server.js
+++ server.js
@@ -46,7 +46,8 @@ app.use(express.static(path.join(__dirname, './client/build/')))
 // })
 
 app.use('*', function(req, res){
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    const index = path.join(__dirname, 'client/build', index.html)
+    res.sendFile(index);
 })
 
 const PORT = process.env.PORT || 8080;
