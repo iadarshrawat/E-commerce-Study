eea65838a52a32d5f69c01c7146799348ff119b0
Adarsh rawat <00adarshrawat00@gmail.com>
2023-08-20 16:50:48 +0530

    again fixing some bug

--- server.js
+++ server.js
@@ -34,7 +34,9 @@ app.use(bodyParser.json())
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/category', categoryRoutes);
 app.use('/api/v1/product', productRoutes);
-app.use(express.static(path.join(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, './client/build/')))
+
+// console.log(path.join(__dirname, ));
 
 // rest api
 // app.get('/', (req, res)=>{
