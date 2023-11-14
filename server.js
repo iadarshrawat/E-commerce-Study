a0c886f20a99edaaa2694445d36d69101e801c17
Adarsh rawat <00adarshrawat00@gmail.com>
2023-11-14 07:24:26 +0530

    changes cors

--- server.js
+++ server.js
@@ -24,9 +24,16 @@ const __dirname = path.dirname(__filename);
 // rest object
 const app  = express();
 
+// cors error removal for production
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 // middelware
-app.use(cors())
+app.use(cors({origin:"*"}))
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
